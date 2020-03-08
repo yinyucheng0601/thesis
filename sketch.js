5d1e526bc0c55b261fc6803d6397e37a570eb7ce
yinyucheng0601 <42951346+yinyucheng0601@users.noreply.github.com>
2020-03-07 21:29:32 -0500

    3.7

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ var objectR =255, objectG = 255, objectB = 255, randomcolor;
 var debug = true;
 var ctx, capture, canvas, imgToy, imgToy2, cat;
 var center;
-
 var prevImgToy1Msg = { aveX: 0, aveY: 0 }, prevImgToy2Msg = { aveX: 0, aveY: 0 };
 
 function screenshot(){
@@ -17,7 +16,7 @@ function screenshot(){
 function preload(){
 	imgToy = loadImage('toy.png');
 	imgToy2 = loadImage('toy.png');
-	cat= loadImage('cat1.gif');
+	cat= loadImage('cat1.png');
   }
 
 function setup() {
@@ -31,7 +30,7 @@ function setup() {
 	frameRate(60);
 
 
-	//randomcolor = "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (85 + 10 * Math.random()) + '%)';
+
 
 }
 
@@ -89,40 +88,12 @@ function draw() {
 	}
 }
 
-function rgbToHsv(r, g, b) {
-	r /= 255, g /= 255, b /= 255;
-
-	var max = Math.max(r, g, b), min = Math.min(r, g, b);
-	var h, s, v = max;
-
-	var d = max - min;
-	s = max == 0 ? 0 : d / max;
-
-	if (max == min) {
-	  h = 0; // achromatic
-	} else {
-	  switch (max) {
-		case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-		case g: h = (b - r) / d + 2; break;
-		case b: h = (r - g) / d + 4; break;
-	  }
-
-	  h /= 6;
-	}
-
-	return [ 100*h, 50*s, 100 , 60 ];
-  }
 
 function handleDraw({ totalFoundPixels, sumX, sumY, objectR, objectB, objectG }) {
 	// average location of pixels
 	aveX = sumX / totalFoundPixels;
 	aveY = sumY / totalFoundPixels;
     prevImgToy1Msg = {aveX: aveX, aveY: aveY}
-
-	// rgbToHsv(objectR,objectG,objectB);
-	// //console.log(rgbToHsv(objectR,objectG,objectB));
-
-	// //fill(objectR,objectG+10,objectB+10, Math.random() * 80);
 	// fill(objectR,objectG,objectB);
 	// noStroke()
 	// var r = Math.random()*50;
